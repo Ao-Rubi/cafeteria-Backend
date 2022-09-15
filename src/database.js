20b33bff19b9695f81560ef8abf5c04a55bf15e3
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-09-15 10:05:18 -0300

    cambio la url de la base de datos

--- src/database.js
+++ src/database.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-const url = "mongodb://localhost:27017/cafeteriac2i"
+// const url = "mongodb://localhost:27017/cafeteriac2i"  // Base de dato local
+const url = "mongodb+srv://Ao-Rubi:PHw1oZrxL4mHy8ry@cluster0.x3upctr.mongodb.net/cafec2i" //Base de dato en la nube
+
 
 mongoose.connect(url);
 
